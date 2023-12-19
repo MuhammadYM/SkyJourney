d77b85516a42065fc3f89053488f5806c6eef032
Muhammad <muhammadalnabhani@gmail.com>
2023-12-19 16:50:46 +0000

    Rename card

--- app/src/main/java/com/example/skyjourney/MainActivity.kt
+++ app/src/main/java/com/example/skyjourney/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DeparturesCard()
+                    FlightDetailsCard()
                 }
             }
         }
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun DeparturesCard(){
+fun FlightDetailsCard(){
     BpkTheme {
          Column {
             BpkCard {
